fd184f3d20a001f93aa635a9030ffc5942ea3d16
Ben Frankel <ben.frankel7@gmail.com>
2018-12-18 01:22:58 +0200

    Initial commit

---
+++ 2018/day1/part1/go/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	f, err := os.Open("../input")
+	check(err)
+	defer f.Close()
+
+	in := bufio.NewScanner(f)
+	freq := 0
+	for in.Scan() {
+		token := in.Text()
+		delta, err := strconv.Atoi(token)
+		check(err)
+		
+		freq += delta
+	}
+
+	fmt.Println(freq)
+	check(in.Err())
+}
---
+++ 2018/day1/part2/go/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func Append(slice []int, n int) []int {
+	l := len(slice)
+
+	if l >= cap(slice) {
+		newSlice := make([]int, cap(slice)*2)
+		copy(newSlice, slice)
+		slice = newSlice
+	}
+
+	slice = slice[0 : l+1]
+	slice[l] = n
+
+	return slice
+}
+
+func main() {
+	freq := 0
+	seen := make(map[int]bool)
+	deltas := make([]int, 0, 10)
+
+	f, err := os.Open("../input")
+	check(err)
+	defer f.Close()
+
+	in := bufio.NewScanner(f)
+
+	for in.Scan() {
+		if seen[freq] {
+			fmt.Println(freq)
+			return
+		}
+		
+		seen[freq] = true
+
+		token := in.Text()
+		delta, err := strconv.Atoi(token)
+		check(err)
+		
+		deltas = Append(deltas, delta)
+		freq += delta
+	}
+	
+	check(in.Err())
+
+	for {
+		for _, delta := range deltas {
+			if seen[freq] {
+				fmt.Println(freq)
+				return
+			}
+			
+			seen[freq] = true
+			freq += delta
+		}
+	}
+}
